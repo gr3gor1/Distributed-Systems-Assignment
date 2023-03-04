2f3bb94b5d37cbc7122fb86ea1e0de3454fd7272
gr3gor1 <gr3gorypapanikolaou@gmail.com>
2023-03-04 12:01:31 +0200

    Update block.py

--- block.py
+++ block.py
@@ -1,19 +1,29 @@
-
+from time import time
+import hashlib
 import blockchain
 
 
 
 
 class Block:
+<<<<<<< Updated upstream
 	def __init__(self):
 		#self.previousHash
 		#self.timestamp
 		#self.hash
 		#self.nonce
 		#self.listOfTransactions
+=======
+	def __init__(self,previousHash):
+		self.previousHash = previousHash
+		self.timestamp = time.time()
+		self.hash = myHash()
+		self.nonce=0
+		self.listOfTransactions=[]
+>>>>>>> Stashed changes
 	
 	def myHash:
-		#calculate self.hash
+		
 
 
 	def add_transaction(transaction transaction, blockchain blockchain):
