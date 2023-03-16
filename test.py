2cb7e99b5d102bfcf80bbff82af2042c9f524f65
gr3gor1 <gr3gorypapanikolaou@gmail.com>
2023-03-16 13:49:35 +0200

    update

---
+++ test.py
@@ -0,0 +1,7 @@
+from block import Block
+from blockchain import Blockchain
+from wallet import Wallet
+from transaction import Transaction
+
+wallet = Wallet()
+print(wallet.stringify())
--- wallet.py
+++ wallet.py
@@ -2,7 +2,7 @@ import binascii
 
 import Crypto
 import Crypto.Random
-from Crypto.Hash import SHA
+from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 
@@ -16,29 +16,29 @@ from uuid import uuid4
 class Wallet:
 
 	def __init__(self):
-		random = Crypto.Random.new().read()
 		#create private key
-		self.private_key = RSA.generate(1024,random)
+		root = RSA.generate(1024)
+		self.private_key = binascii.hexlify(root.exportKey()).decode()
 		#create public key
-		self.public_key = self.private_key.publickey()
-		#hex public key
-		self.public_key_hex = hashlib.sha256(self.public_key.exportKey(format='DER')).hexdigest()
-		self.private_key_hex = hashlib.sha256(self.private_key.exportKey(format='DER')).hexdigest()
+		self.public_key = hashlib.sha256(root.publickey().exportKey(format='DER')).digest().hex()
 		#wallet address is equal to the SHA256 hash of the public key in hexadecimal format
-		self.address = self.public_key_hex
+		self.address = self.public_key
 		#transaction of the wallet
 		self.transactions = []
 
 
-		def balance(self):
-			#sum of the UTXOs according to the assignment
-			sum = 0
-			for transaction in self.transactions:
-				for out in transaction.transaction_out :
-					if out.unspent and out.recipient_address == self.public_key:
-						sum += out.amount
+	def balance(self):
+		#sum of the UTXOs according to the assignment
+		sum = 0
+		for transaction in self.transactions:
+			for out in transaction.transaction_out :
+				if out.unspent and out.recipient_address == self.public_key:
+					sum += out.amount
 
-			return sum
+		return sum
+		
+	def stringify(self):
+		return str(self.__dict__)
 
 
 			
